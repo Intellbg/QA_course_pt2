3a20f61bdfee045b59982ad0efc29300b77acf99
Glitch (chatter-elated-manta) <none>
2024-01-23 15:04:06 +0000

    🍍🎬 Checkpoint ./server.js:64361580/16453

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ myDB(async (client) => {
       title: "Connected to Database",
       message: "Please login",
       showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -71,6 +72,15 @@ myDB(async (client) => {
     res.render("profile", { username: req.user.username });
   });
 
+  app.route("/register").get(ensureAuthenticated, (req, res) => {
+    res.render("profile", { username: req.user.username });
+  });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
@@ -81,19 +91,13 @@ myDB(async (client) => {
     });
   });
 
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
   });
-  
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("index", { title: e, message: "Unable to connect to database" });
   });
-});
-
-app.use((req, res, next) => {
-  res.status(404).type("text").send("Not Found");
 });
 
 const PORT = process.env.PORT || 3000;
