af712f47b3d8150fedc386b44b531047f02e3cfb
Glitch (chatter-elated-manta) <none>
2024-01-23 23:42:22 +0000

    🌄🚵 Checkpoint ./auth.js:64361580/8724 ./routes.js:64361580/227

--- routes.js
+++ routes.js
@@ -26,7 +26,8 @@ module.exports = function (app, myDataBase) {
     .get(
       passport.authenticate("github", { failureRedirect: "/" }),
       (req, res) => {
-        res.redirect("/profile");
+        req.session.user_id = req.user.id
+        res.redirect("/chat");
       }
     );
 
@@ -41,6 +42,10 @@ module.exports = function (app, myDataBase) {
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
     res.render("profile", { username: req.user.username });
+  });
+  
+  app.route("/chat ").get(ensureAuthenticated, (req, res) => {
+    res.render("chat", { username: req.user });
   });
 
   app.route("/register").post((req, res, next) => {
