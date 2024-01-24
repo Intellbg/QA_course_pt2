769306453f907b8f1fa466c4ea940790769edc51
Glitch (chatter-elated-manta) <none>
2024-01-24 01:18:25 +0000

    🤑⛷ Checkpoint ./server.js:64361580/15762

--- server.js
+++ server.js
@@ -7,24 +7,30 @@ const myDB = require("./connection");
 const routes = require("./routes.js");
 const auth = require("./auth.js");
 const passport = require("passport");
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
 
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require("connect-mongo")(session);
 const URI = process.env.MONGO_URI;
 const store = new MongoStore({ url: URI });
 
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-io.use(
-  passportSocketIo.authorize({
-    cookieParser: cookieParser,
-    key: 'express.sid',
-    secret: process.env.SESSION_SECRET,
-    store: store,
-    success: onAuthorizeSuccess,
-    fail: onAuthorizeFail
-  })
-);
+
+
+function onAuthorizeSuccess(data, accept) {
+  console.log("successful connection to socket.io");
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log("failed connection to socket.io:", message);
+  accept(null, false);
+}
+
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 fccTesting(app);
@@ -38,20 +44,36 @@ app.use(
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false },
+    key: "express.sid",
+    store: store,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "express.sid",
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
+  })
+);
+
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
   auth(app, myDataBase);
   routes(app, myDataBase);
+
   let currentUsers = 0;
   io.on("connection", (socket) => {
     ++currentUsers;
     console.log("A user has connected");
     io.emit("user count", currentUsers);
+    console.log("user " + socket.request.user.username + " connected");
+    
     socket.on("disconnect", () => {
       --currentUsers;
       io.emit("user count", currentUsers);
